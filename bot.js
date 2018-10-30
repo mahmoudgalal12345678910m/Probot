e385d28572b3828c0038a57aeee1fbd1c56b3da8
kingofmenewwork <43719252+mahmoudgalal12345678910m@users.noreply.github.com>
2018-10-31 01:08:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -399,6 +399,7 @@ client.on('ready', function(){
 
 
 
+
 if (roles.length !== reactions.length) throw "Roles list and reactions list are not the same length!";
 
 function generateMessages(){
@@ -426,16 +427,16 @@ client.on("message", message => {
 client.on('raw', event => {
     if (event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
         
-        let channel = bot.channels.get(event.d.channel_id);
+        let channel = client.channels.get(event.d.channel_id);
         let message = channel.fetchMessage(event.d.message_id).then(msg=> {
         let user = msg.guild.members.get(event.d.user_id);
         
-        if (msg.author.id == bot.user.id && msg.content != initialMessage){
+        if (msg.author.id == client.user.id && msg.content != initialMessage){
        
             var re = `\\*\\*"(.+)?(?="\\*\\*)`;
             var role = msg.content.match(re)[1];
         
-            if (user.id != bot.user.id){
+            if (user.id != client.user.id){
                 var roleObj = msg.guild.roles.find(r => r.name === role);
                 var memberObj = msg.guild.members.get(user.id);
                 
@@ -450,9 +451,6 @@ client.on('raw', event => {
  
     }   
 });
-
-
-
 
 
 
